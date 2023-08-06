d78f7c9c3a53f4c80137bec0a037849d38773029
Dariusz <dariusz@ridely.com>
2023-08-06 11:56:40 +0200

    first commit

---
+++ MidiPlayer/MidiPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidiPlayerApp.swift
+//  MidiPlayer
+//
+//  Created by Dariusz Zabrzeński on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MidiPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
